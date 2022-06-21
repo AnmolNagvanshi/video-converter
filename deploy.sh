ec5e7ec369b5a6c2a994d16b00768b7c1e63172b
Anmol Nagvanshi <anmolnagvanshi04@gmail.com>
2022-06-21 10:23:23 +0530

    added deploy file

---
+++ deploy.sh
@@ -0,0 +1,6 @@
+source /home/ubuntu/video-converter/venv/bin/activate
+git pull
+pip install -r requirements.txt
+python3 manage.py migrate
+sudo service gunicorn restart
+sudo service nginx restart
